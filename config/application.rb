e0b5070083f72f1777b6960f7a3ea698336ba670
拓人馬場 <patashu0919@gmail.com>
2017-07-11 15:44:02 +0900

    setting for gem

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module SurveyCompany
   class Application < Rails::Application
-  config.generators do |g|
-    g.test_framework false
-    g.helper false
-    g.assets false
+    config.generators do |g|
+      g.test_framework false
+      g.helper false
+      g.assets false
+    end
   end
 end
