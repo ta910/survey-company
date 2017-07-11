3692c3055636750579df613af8f910c3eaa9b239
拓人馬場 <patashu0919@gmail.com>
2017-07-11 17:07:33 +0900

    setting for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  namespace :admins do
+    resources :campanies
+    resources :surveys, only: [:new, :create] do
+      resources :questions, only: [:new, :create]
+    end
+  end
+
+  namespace :mains do
+    resources :users, only: [:index, :delete]
+  end
+
+  resources :companies, only: [] do
+    resources :survey, only: [:index, :show] do
+      member do
+        get :answer_new
+        post :answer_create
+      end
+    end
+  end
+
+  resources :users, only: :show do
+    resources :messages, only: [:new, :create]
+  end
+
 end
+
