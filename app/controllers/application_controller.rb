9f3960fbea90e8760c329df50b584ea18847cedd
拓人馬場 <patashu0919@gmail.com>
2017-07-12 16:37:57 +0900

    edit devise parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :company_id])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:company_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 
 end
