f7e8e56728753b54c341b482ddb37872bcfd3b52
拓人馬場 <patashu0919@gmail.com>
2017-07-13 20:28:02 +0900

    correct style

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,9 +4,7 @@ class SurveysController < ApplicationController
 
   def index
     user_ids = Company.find_by(name: params[:company_name]).users.ids
-    unless user_ids.include?(current_user.id)
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_ids.include?(current_user.id)
   end
 
 end
