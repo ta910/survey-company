916e0eecbefb5e3cf7ff5d4614fd9a771bb79fd6
拓人馬場 <patashu0919@gmail.com>
2017-07-13 20:02:51 +0900

    can't see other company

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -3,7 +3,10 @@ class SurveysController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    user_ids = Company.find_by(name: params[:company_name]).users.ids
+    unless user_ids.include?(current_user.id)
+      redirect_to root_path
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   def index
     unless current_user.main?
       redirect_to root_path
+    else
+      user_ids = Company.find_by(name: params[:company_name]).users.ids
+      unless user_ids.include?(current_user.id)
+        redirect_to root_path
+      end
     end
   end
 
