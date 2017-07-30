78fe4d864ab40c7072153237a6b9eb4febdd3145
拓人馬場 <patashu0919@gmail.com>
2017-07-30 23:17:44 +0900

    change controller

--- app/controllers/admins/companies_controller.rb
+++ app/controllers/admins/companies_controller.rb
@@ -4,11 +4,25 @@ class Admins::CompaniesController < AdminsController
     @companies = Company.all
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+  end
+
   def search
     @companies = Company.where('name LIKE ?', "%#{params[:name]}%")
     respond_to do |format|
       format.json { render json: @companies }
     end
+  end
+
+  private
+
+  def company_params
+    # params.require(:company).permit(:)
   end
 
 end
