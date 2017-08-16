47cacc43cc54491d5423988b4138091a8902db8b
拓人馬場 <patashu0919@gmail.com>
2017-08-16 17:38:51 +0900

    make index

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,3 +1,15 @@
 class SurveysController < ApplicationController
   before_action :authenticate_user!, :authorized_user!
+
+  def index
+    @surveys = Survey.order(created_at: 'DESC').page(index_params[:page]).per(index_params[:per])
+  end
+
+  private
+
+    def index_params
+      @index_params = params.permit(:page, :per)
+      @index_params = @index_params.merge(per: 5) if params[:per].blank?
+      @index_params
+    end
 end
