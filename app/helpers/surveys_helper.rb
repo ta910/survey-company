fa8894aa90c4edc9b94053af24cddb925338233e
拓人馬場 <patashu0919@gmail.com>
2017-08-18 20:29:42 +0900

    correct

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,5 +1,5 @@
 module SurveysHelper
   def done_user(survey, company)
-    SurveyProgress.where(user_id: company.users, survey: survey).length
+    SurveyProgress.where(status: 'done', user_id: company.users, survey: survey).length
   end
 end
